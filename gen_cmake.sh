06b9a2b6590040aec83a660ba3a2c8b0238fc28f
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2022-08-03 12:21:23 +0200

    Added gen_cmake options to enable C and C# bindings libraries

--- gen_cmake.sh
+++ gen_cmake.sh
@@ -14,5 +14,46 @@ if [ -f "${selfFolderPath}.defaults.sh" ]; then
 	. "${selfFolderPath}.defaults.sh"
 fi
 
+gen_c=0
+gen_csharp=0
+
+function extend_gc_fnc_help()
+{
+	echo " -build-c -> Build the C bindings library."
+	echo " -build-csharp -> Build the C# bindings library."
+}
+
+function extend_gc_fnc_unhandled_arg()
+{
+	case "$1" in
+		-build-c)
+			gen_c=1
+			return 1
+			;;
+		-build-csharp)
+			gen_csharp=1
+			return 1
+			;;
+	esac
+	return 0
+}
+
+# function extend_gc_fnc_postparse()
+# {
+# }
+
+function extend_gc_fnc_precmake()
+{
+	if [ $gen_c -eq 1 ]; then
+		add_cmake_opt+=("-DBUILD_C_BINDINGS=TRUE")
+		add_cmake_opt+=("-DINSTALL_NIH_LIB_C=TRUE")
+	fi
+	if [ $gen_csharp -eq 1 ]; then
+		add_cmake_opt+=("-DBUILD_NIH_SWIG=TRUE")
+		add_cmake_opt+=("-DINSTALL_NIH_LIB_SWIG=TRUE")
+		add_cmake_opt+=("-DNIH_SWIG_LANGUAGES=csharp")
+	fi
+}
+
 # execute gen_cmake script from bashUtils
 . "${selfFolderPath}scripts/bashUtils/gen_cmake.sh"
