2289aaf6af9b9a05e1120f18dcf43184ee19380f
Christophe Calmejane <christophe.calmejane@l-acoustics.com>
2022-08-03 12:12:58 +0200

    Added .defaults file

--- gen_cmake.sh
+++ gen_cmake.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 # Useful script to generate project files using cmake
 
+# Override default cmake options
 cmake_opt="-DBUILD_NIH_EXAMPLES=TRUE -DBUILD_NIH_TESTS=TRUE -DBUILD_C_BINDINGS=TRUE -DBUILD_NIH_SWIG=FALSE -DINSTALL_NIH_EXAMPLES=TRUE -DINSTALL_NIH_TESTS=TRUE -DINSTALL_NIH_LIB_STATIC=TRUE -DINSTALL_NIH_LIB_C=TRUE -DINSTALL_NIH_LIB_SWIG=TRUE -DINSTALL_NIH_HEADERS=TRUE -DENABLE_CODE_SIGNING=FALSE"
 
 ############################ DO NOT MODIFY AFTER THAT LINE #############
 
 # Get absolute folder for this script
 selfFolderPath="`cd "${BASH_SOURCE[0]%/*}"; pwd -P`/" # Command to get the absolute path
+
+# Include default values
+if [ -f "${selfFolderPath}.defaults.sh" ]; then
+	. "${selfFolderPath}.defaults.sh"
+fi
 
 # execute gen_cmake script from bashUtils
 . "${selfFolderPath}scripts/bashUtils/gen_cmake.sh"
